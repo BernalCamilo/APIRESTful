971620b50b4828dad4df406fa575207c7cfff599
BernalCamilo <jc_bernal2011@hotmail.com>
2025-06-19 23:31:25 -0500

    fix: usar puerto dinámico para Heroku

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
   res.send('API funcionando correctamente');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
